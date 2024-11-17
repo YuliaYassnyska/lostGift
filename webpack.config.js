f5a98b353a6a273a67c733b0f21b2e734c011296
YuliaYassnyska <yuliayasso@gmail.com>
2024-11-18 00:08:30 +0200

    project init

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import webpack from "webpack";
+
+export default {
+    mode: "development",
+    devtool: "eval-source-map",
+    entry: "./src/game.js",
+    output: {
+        path: path.resolve(process.cwd(), 'dist'),
+        filename: "bundle.min.js"
+    },
+    resolve: {
+        extensions: [".ts", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader"
+            },
+            {
+                test: [/\.vert$/, /\.frag$/],
+                use: "raw-loader"
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|xml|glsl)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [path.join(process.cwd(), "dist/**/*")]
+        }),
+        new webpack.DefinePlugin({
+            "typeof CANVAS_RENDERER": JSON.stringify(true),
+            "typeof WEBGL_RENDERER": JSON.stringify(true),
+            "typeof WEBGL_DEBUG": JSON.stringify(true),
+            "typeof EXPERIMENTAL": JSON.stringify(true),
+            "typeof PLUGIN_CAMERA3D": JSON.stringify(false),
+            "typeof PLUGIN_FBINSTANT": JSON.stringify(false),
+            "typeof FEATURE_SOUND": JSON.stringify(true)
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            favicon: "./favicon.ico"
+        })
+    ]
+};
