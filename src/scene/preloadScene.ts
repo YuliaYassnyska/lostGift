9bc20e8ecb590e719acd51474d4d068e5f695024
YuliaYassnyska <yuliayasso@gmail.com>
2024-11-19 18:14:23 +0200

    config assets

--- src/scene/preloadScene.ts
+++ src/scene/preloadScene.ts
@@ -6,20 +6,19 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    const images = ['background']
-    images.forEach(img => {
+    const mapElements = ['1', '2', '3', '4', '5', '6', '14', '15', '16',];
+    mapElements.forEach(img => {
+      this.load.spritesheet(img, `assets/${img}.png`, { frameHeight: 100, frameWidth: 100 });
+    });
+
+    const santaElements = ['SantaWalk-1', 'SantaWalk-2', 'SantaWalk-3', 'SantaWalk-4', 'SantaWalk-5', 'SantaWalk-6', 'SantaWalk-7', 'SantaWalk-8'];
+    santaElements.forEach(img => {
       this.load.image(img, `assets/${img}.png`)
-    })
+    });
 
     this.load.image('background', `assets/background.jpg`);
-    
-    // this.load.spritesheet('player', 'assets/img/player.png', { frameHeight: 165, frameWidth: 120 })
-    // this.load.spritesheet('coin', 'assets/img/coin.png', { frameHeight: 42, frameWidth: 42 })
-    // this.load.spritesheet('bee', 'assets/img/bee.png', { frameHeight: 100, frameWidth: 128 })
-    // this.load.spritesheet('slime', 'assets/img/slime.png', { frameHeight: 68, frameWidth: 112 })
-    // this.load.setPath('assets/spine')
-    // // @ts-ignore
-    // this.load.spine('boy', 'boy.json', 'boy.atlas')
+    this.load.image('santa', `assets/Santa.png`);
+    this.load.image('gift', `assets/gift.png`);
   }
 
   create() {
