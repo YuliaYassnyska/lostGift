94c26f098743fa755915623bfad66ed83d6862a9
YuliaYassnyska <yuliayasso@gmail.com>
2024-11-19 18:15:01 +0200

    add simple level

---
+++ src/components/levels.ts
@@ -0,0 +1,12 @@
+type Level = string[]
+
+const level0: Level = [
+    '     G         ',
+    '  [////]       ',
+    '               ',
+    '   S         G ',
+    '[/////]   [///]',
+]
+
+const Levels: Level[] = [level0,]
+export default Levels
