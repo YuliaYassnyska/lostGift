c75bebc09774ee2e73821cd6ad6aff0d145b708d
YuliaYassnyska <yuliayasso@gmail.com>
2024-11-22 19:18:06 +0200

    fix tank

--- src/components/enemiesGroup.ts
+++ src/components/enemiesGroup.ts
@@ -24,15 +24,15 @@ export default class EnemiesGroup extends Phaser.GameObjects.Group {
   update() {
     this.children.iterate((enemy: Phaser.GameObjects.GameObject) => {
       if (!(enemy instanceof TankSprite)) return true;
-  
+
       const body = enemy.body as Phaser.Physics.Arcade.Body;
-  
-      if (enemy.dead) return true; 
-  
-      let enemyIsMovingRight = body.velocity.x >= 0;
-  
+
+      if (enemy.dead) return true;
+
+      let enemyIsMovingRight = body.velocity.x <= 0;
+
       let hasGroundDetection = this.tiles.filter(tile => {
-        let enemyPositionX = enemyIsMovingRight ? body.right : body.left;
+        let enemyPositionX = enemyIsMovingRight ? body.left : body.right;
         let x =
           enemyPositionX + 32 > tile.x &&
           enemyPositionX - 32 < tile.x + this.TILE_SIZE;
@@ -41,14 +41,14 @@ export default class EnemiesGroup extends Phaser.GameObjects.Group {
           body.bottom + this.TILE_SIZE / 2 < tile.y + this.TILE_SIZE;
         return x && y;
       });
-  
-     
+
+
       if (hasGroundDetection.length === 0) {
-        body.setVelocityX(body.velocity.x * -1); 
-        enemy.setFlipX(!enemyIsMovingRight);   
+        body.setVelocityX(body.velocity.x * -1);
+        enemy.setFlipX(!enemyIsMovingRight);
       }
-  
-      return true; 
+
+      return true;
     });
   }
 }
