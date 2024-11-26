212720127570b0a40871afc364d2b762a4ee5857
YuliaYassnyska <yuliayasso@gmail.com>
2024-11-26 16:55:02 +0200

    fix snowman

--- src/components/snowman.ts
+++ src/components/snowman.ts
@@ -1,7 +1,8 @@
 import EnemyClass from './enemyClass';
 
 export default class SnowmanSprite extends EnemyClass {
-  text: Phaser.GameObjects.Text; // Текстове повідомлення
+  text: Phaser.GameObjects.Text;
+  isAppearing: boolean = false;
 
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'snowman');
@@ -15,14 +16,20 @@ export default class SnowmanSprite extends EnemyClass {
     this.body.setOffset(20, 20);
 
     this.dead = false;
-    this.alpha = 0; 
+    this.alpha = 0;
 
     this.appearDisappearCycle();
   }
 
   appear() {
-    this.alpha = 0; 
-    this.setPosition(Phaser.Math.Between(100, this.scene.cameras.main.width - 100), Phaser.Math.Between(100, this.scene.cameras.main.height - 100));
+    if (this.dead || this.isAppearing) return; 
+    this.isAppearing = true;
+
+    this.alpha = 0;
+    this.setPosition(
+      Phaser.Math.Between(100, this.scene.cameras.main.width - 100),
+      Phaser.Math.Between(100, this.scene.cameras.main.height - 100)
+    );
 
     this.text = this.scene.add.text(this.x, this.y - 50, 'Сніговик слідкує за тобою', {
       font: '20px Arial',
@@ -30,7 +37,7 @@ export default class SnowmanSprite extends EnemyClass {
       fill: '#BFECFF',
       stroke: '#000000',
       strokeThickness: 4,
-      align: 'center'
+      align: 'center',
     }).setOrigin(0.5);
 
     this.scene.tweens.add({
@@ -38,40 +45,46 @@ export default class SnowmanSprite extends EnemyClass {
       alpha: { from: 0, to: 1 },
       duration: 1000,
       onStart: () => {
-        this.body.enable = true; 
+        this.body.enable = true;
+      },
+      onComplete: () => {
+        this.isAppearing = false; 
       },
     });
 
     this.scene.time.addEvent({
-      delay: 2000, 
+      delay: 2000,
       callback: () => {
-        if (this.text) this.text.destroy(); 
-      }
+        if (this.text) this.text.destroy();
+      },
     });
   }
 
   disappear() {
+    if (this.dead || this.isAppearing) return; 
+    this.isAppearing = true;
+
     this.scene.tweens.add({
       targets: this,
       alpha: { from: 1, to: 0 },
       duration: 1000,
       onComplete: () => {
-        this.body.enable = false; 
+        this.body.enable = false;
+        this.isAppearing = false; 
       },
     });
   }
 
-  
   appearDisappearCycle() {
     this.scene.time.addEvent({
-      delay: Phaser.Math.Between(2000, 6000), 
+      delay: Phaser.Math.Between(2000, 6000),
       callback: () => {
         if (!this.dead) {
-          this.appear(); 
+          this.appear();
           this.scene.time.addEvent({
-            delay: 5000, 
+            delay: 5000,
             callback: () => {
-              if (!this.dead) this.disappear(); 
+              if (!this.dead) this.disappear();
             },
           });
         }
@@ -83,7 +96,12 @@ export default class SnowmanSprite extends EnemyClass {
   kill() {
     if (this.dead) return;
     this.dead = true;
-    this.setFrame(30); 
+
+    this.isAppearing = false;
+
+    if (this.text) this.text.destroy();
+    this.setFrame(30);
     this.removeEnemy();
   }
 }
+
