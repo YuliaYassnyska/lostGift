b6d302b25b0284790055f20f2cb70ae4438ea15c
YuliaYassnyska <yuliayasso@gmail.com>
2024-12-08 22:34:24 +0200

    update deer

--- src/components/reindeer.ts
+++ src/components/reindeer.ts
@@ -5,12 +5,35 @@ export default class Reindeer extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
-    this.setScale(0.15);
-    this.setOrigin(0, 0.5);
+    this.setScale(0.08);
+    this.setOrigin(0, 0.3);
+    this.setFrame(10);
     (this.body as Phaser.Physics.Arcade.Body).setAllowGravity(false);
   }
 
   triggerMagicEffect() {
     this.scene.cameras.main.flash(500, 255, 255, 255);
+
+    const blindEffect = this.scene.add.rectangle(
+      0,
+      0,
+      this.scene.scale.width + 10,
+      this.scene.scale.height + 10,
+      0x000000,
+      1
+    );
+    blindEffect.setOrigin(0, 0);
+    blindEffect.setScrollFactor(0);
+    blindEffect.setDepth(1000);
+
+    this.scene.tweens.add({
+      targets: blindEffect,
+      alpha: 0,
+      duration: 1000,
+      delay: 3000,
+      onComplete: () => {
+        blindEffect.destroy();
+      },
+    });
   }
 }
